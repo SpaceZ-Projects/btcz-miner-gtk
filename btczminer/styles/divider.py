29ec285b1b1df4635da9ee8a2428c1ee8ea2301d
EzzyG <dragkhai@gmail.com>
2024-07-21 19:08:52 +0100

    None

---
+++ btczminer/styles/divider.py
@@ -0,0 +1,9 @@
+from toga.style.pack import Pack
+from toga.constants import CENTER
+
+class DividerStyle():
+    
+    
+    download_divider_top = Pack(
+        padding_bottom = 5
+    )
---
+++ btczminer/styles/selection.py
@@ -0,0 +1,33 @@
+from toga.style.pack import Pack
+from toga.colors import BLACK, YELLOW
+
+
+class SelectionStyle():
+    
+    
+    select_miner = Pack(
+        padding_top = 15,
+        padding_left = 20,
+        width = 250,
+        font_size = 11,
+        color = YELLOW,
+        background_color = BLACK
+    )
+
+    select_pool = Pack(
+        padding_top = 15,
+        padding_left = 25,
+        width = 250,
+        font_size = 11,
+        color = YELLOW,
+        background_color = BLACK
+    )
+
+    select_server = Pack(
+        padding_top = 15,
+        padding_left = 60,
+        width = 200,
+        font_size = 11,
+        color = YELLOW,
+        background_color = BLACK
+    )
